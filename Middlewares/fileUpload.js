7e86a2a86ad8b147fecfc9fbe5cfe7b37f4a1840
kamran rahmani <rahmanikamran95@gmail.com>
2023-04-03 19:07:36 -0400

    try to fix gcs err

--- Middlewares/fileUpload.js
+++ Middlewares/fileUpload.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const multer = require('multer');
 const {Storage} = require('@google-cloud/storage');
 
-const storage = new Storage({projectId: process.env.PROJECT_ID, credentials:{client_email: process.env.CLIENT_EMAIL, private_key: process.env.PRIVATE_KEY}});
+const storage = new Storage({projectId: process.env.PROJECT_ID, credentials:{client_email: process.env.CLIENT_EMAIL, private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n')}});
 
 const bucket = storage.bucket(process.env.BUCKET_NAME);
 
